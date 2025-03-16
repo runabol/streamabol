71a9de0c218a1d584bb4f70c231728e588f0d563
Arik Cohen <me@arikcohen.com>
2025-03-16 11:21:59 -0400

    Removed redundant code

--- handlers/video.go
+++ handlers/video.go
@@ -33,14 +33,6 @@ type ProbeResult struct {
 }
 
 func Video(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	if r.Method == http.MethodOptions {
-		w.WriteHeader(http.StatusOK)
-		return
-	}
-
 	src := r.URL.Query().Get("src")
 	if src == "" {
 		http.Error(w, "Missing src parameter", http.StatusBadRequest)
